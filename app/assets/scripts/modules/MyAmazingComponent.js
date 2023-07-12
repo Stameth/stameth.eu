6fdd43d121ae946e6a7f7de510a0c5e1902489c6
Stamatis <stameth96@gmail.com>
2023-07-12 13:07:26 +0300

    Added a copyright to the footer

---
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MyAmazingComponent() {
+    return (
+      <div>
+        <h1 className="section-title section-title--blue">This is my Amazing React Component</h1>
+        <p  className="centered-paragraph">I made this section using React.</p>
+      </div>
+    )
+  }
+
+  export default MyAmazingComponent
